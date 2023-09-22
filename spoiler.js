15d608ae507be814e0f92535c23f9bb352f6e3d3
Danila-alinaD <126895741+Danila-alinaD@users.noreply.github.com>
2023-09-22 13:44:27 +0300

    Add files via upload

--- spoiler.js
+++ spoiler.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const spoilerButton = document.querySelector(".spoiler-button");
-    const spoilerContent = document.querySelector(".spoiler-content");
+    const spoilerButton = document.querySelector(".spoiler-button-c");
+    const spoilerContent = document.querySelector(".spoiler-content-c");
 
     spoilerButton.addEventListener("click", function () {
         if (spoilerContent.style.display === "none" || spoilerContent.style.display === "") {
